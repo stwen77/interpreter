d074a42bf2ed31b8ddccc9d2581f8f6c19aead66
Shitong Wen <shitwen@qti.qualcomm.com>
2019-07-03 20:27:46 +0800

    add next and inner next for token iterator, add num parse

--- src/module/parser.rs
+++ src/module/parser.rs
@@ -108,20 +108,18 @@ pub fn parse(input: &mut str) {
     //let mut fn_def = Vec::new();
 
     let mut token_iterator = TokenIterator::new(input);
-    while let Some(_) = token_iterator.char_stream.peek() {
+    while let Some(a) = token_iterator.char_stream.peek() {
+        //println!("{}",a);
         match token_iterator.last {
             Token::Fn => (),
             _ => {
                 parse_statement(&mut token_iterator);
             }
         }
-        token_iterator.char_stream.next();
+        token_iterator.next();
     }
-    println!("");
 }
-pub fn parse_statement(
-    input: &mut TokenIterator
-) -> Result<Statment, ()> {
+pub fn parse_statement(input: &mut TokenIterator) -> Result<Statment, ()> {
     match input.last {
         Token::If => Err(()),
         Token::While => Err(()),
@@ -134,21 +132,70 @@ pub fn parse_statement(
     }
 }
 fn parse_var<'a>(input: &mut Peekable<Chars<'a>>, last_token: &mut Token) {}
-pub struct TokenIterator<'a>{
+pub struct TokenIterator<'a> {
     last: Token,
     char_stream: Peekable<Chars<'a>>,
 }
 impl<'a> TokenIterator<'a> {
-    fn new(input: &'a str) -> Self{
-        TokenIterator{
+    fn new(input: &'a str) -> Self {
+        TokenIterator {
             last: Token::Nothing,
             char_stream: input.chars().peekable(),
         }
     }
-    fn next_token(&mut self, input: &mut Peekable<Chars>) {
+    fn next(&mut self) {
+        self.last =  match self.inner_next() {
+            Some(c) => c,
+            None => Token::Nothing,
+        };
 
     }
-    fn inner_next(&mut self, input: &mut Peekable<Chars>) {
-        
+    fn inner_next(&mut self) -> Option<Token> {
+        while let Some(c) = self.char_stream.next() {
+            match c {
+                '0'...'9' => {
+                    let mut result = Vec::new();
+                    let mut radix_base: Option<u32> = None;
+                    result.push(c);
+
+                    while let Some(&nxt) = self.char_stream.peek() {
+                        match nxt {
+                            '0'...'9' => {
+                                result.push(nxt);
+                                self.char_stream.next();
+                            }
+                            '.' => {
+                                result.push(nxt);
+                                self.char_stream.next();
+                                while let Some(&nxt_float) = self.char_stream.peek() {
+                                    match nxt_float {
+                                        '0'...'9' => {
+                                            result.push(nxt_float);
+                                            self.char_stream.next();
+                                        }
+                                        _ => break,
+                                    }
+                                }
+                            }
+                            _ => break,
+                        }
+                    }
+
+                    let out:String = result.into_iter().collect();
+                    if let Ok(val) = out.parse::<i64>() {
+                        println!("{}", val);
+                        return Some(Token::IntConst(val));
+                    } else if let Ok(val) = out.parse::<f64>() {
+                        println!("{}", val);
+                        return Some(Token::FloatConst(val));
+                    }
+                    return Some(Token::LexErr);
+                }
+
+                _x if _x.is_whitespace() => (),
+                _ => return Some(Token::LexErr),
+            }
+        }
+        None
     }
 }
